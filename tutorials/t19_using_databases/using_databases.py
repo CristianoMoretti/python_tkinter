f941212a013615b93591b114ca3acc35f20a4b09
Cristiano Moretti <cristianomoretti05@gmail.com>
2022-09-05 22:54:22 +0100

    third commit

--- tutorials/t19_using_databases/using_databases.py
+++ tutorials/t19_using_databases/using_databases.py
@@ -5,11 +5,31 @@ import sqlite3
 
 root = Tk()
 root.title("Learn To Code at codemy.com")
-root.iconbitmap('C:/temp/python/tkinter_tutorials/tutorials/t8_images/icon2.ico')
+# root.iconbitmap('C:/temp/python/tkinter_tutorials/tutorials/t8_images/icon2.ico')
 root.geometry("400x400")
 
 # databases
-conn = sqlite3.connect('address_book.db')
+conn = sqlite3.connect('./t19_using_databases/address_book.db')
+
+# create a cursor
+c = conn.cursor()
+
+# create a table
+c.execute("""CREATE TABLE addresses(
+    first_name text,
+    last_name text, 
+    address text, 
+    city text,
+    state text,
+    zipcode integer
+)
+""")
+
+# Commit changes
+conn.commit()
+
+# Close conneciton
+conn.close()
 
 root.mainloop()
 
